7317c5f098d72cc1460317de2efeb69a048dd3eb
moonflaked <loganluo288@gmail.com>
2023-12-13 00:05:34 -0500

    Updated database and category page to include database querying functionality

--- lib/category_model.dart
+++ lib/category_model.dart
@@ -1,19 +1,34 @@
-class Category{
+
+import 'package:sqflite/sqflite.dart';
+
+import 'database.dart';
+
+class Category {
   int? categoryId;
   String? categoryName;
 
-  Category(this.categoryId,this.categoryName);
+  Category(this.categoryId, this.categoryName);
 
-  Category.fromMap(Map<dynamic,dynamic> map){
+  Category.fromMap(Map<String, dynamic> map){
     categoryId = map['category_id'];
     categoryName = map['category_name'];
   }
 
-  Map<dynamic, dynamic> toMap(){
-    return{
-      "category_id" : categoryId,
-      "category_name" : categoryName
+  Map<String, dynamic> toMap() {
+    return {
+      "category_id": categoryId,
+      "category_name": categoryName
     };
   }
 
+  static Future<List<Category>?> retrieveCategories() async {
+    Database? db = await PetMatchDatabase.getInstance();
+
+
+    List<Map<String, Object?>>? listOfCategoryMaps = await db?.query("Category");
+
+    List<Category>? listOfCategories = listOfCategoryMaps?.map((categoryMap) => Category.fromMap(categoryMap)).toList();
+
+    return listOfCategories;
+  }
 }
